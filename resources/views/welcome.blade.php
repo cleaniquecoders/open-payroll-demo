efbe7ec4311a53a364edbe4f9f719b559d5a2729
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-19 11:47:14 +0800

    Added logo to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img width="450px" src="{{ asset('img/OpenPayroll.png') }}" alt="{{ config('app.name') }}">
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://open-payroll.cleaniquecoders.com">Demo Project</a>
+                    <a href="https://github.com/cleaniquecoders/open-payroll">The Package</a>
                 </div>
             </div>
         </div>
